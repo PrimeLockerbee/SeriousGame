af2747ab1b2a833e20ea5d60c7d2d26a688801f8
LuigiOvertoom <firedream2236@gmail.com>
2018-11-19 13:29:10 +0100

    changed map structure

--- Assets/Dev/Bradley/prefabs/temp.cs
+++ Assets/Dev/Bradley/prefabs/temp.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tempfiles : MonoBehaviour {
+public class temp : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
