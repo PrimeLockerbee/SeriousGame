e1fbf8b5191f42e4edc0a6a9061663c11ffd45ec
Bradley van Ewijk <527644@student.glu.nl>
2018-11-23 15:24:34 +0100

    Added an temporary background for the main menu, new folder for temp art is added under my own dev folder.
    Signed-off-by: Bradley van Ewijk <527644@student.glu.nl>

--- Assets/Art/Lili/temp.cs
+++ Assets/Art/Lili/temp.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class temp : MonoBehaviour {
+public class temp6 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
