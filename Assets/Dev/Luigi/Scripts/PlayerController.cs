2256ce7a0fea6e31af3950f308f6a1fc8bd7c75b
LuigiOvertoom <firedream2236@gmail.com>
2018-12-10 14:48:21 +0100

    Auto stash before merge of "bradley" and "origin/bradley"

--- Assets/Dev/Luigi/Scripts/PlayerController.cs
+++ Assets/Dev/Luigi/Scripts/PlayerController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< Updated upstream:Assets/Dev/Luigi/Scripts/temp.cs
 public class temp1 : MonoBehaviour {
+=======
+public class PlayerController : MonoBehaviour {
+>>>>>>> Stashed changes:Assets/Dev/Luigi/Scripts/PlayerController.cs
 
 	// Use this for initialization
 	void Start () {
