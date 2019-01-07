14da61f9850ad0dc13c9319dd6c6e2c9e37c1f1f
Bradley van Ewijk <527644@student.glu.nl>
2019-01-07 15:12:26 +0100

    Stuff Added
    Signed-off-by: Bradley van Ewijk <527644@student.glu.nl>

---
+++ Assets/Dev/Bradley/Scripts/GameTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameTimer : MonoBehaviour
+{
+    float m_GameTime = 60.0f;
+
+    [SerializeField]
+    private TextMeshProUGUI m_TimerText;
+
+    void Update()
+    {
+        m_GameTime -= Time.deltaTime;
+
+        m_TimerText.text = m_GameTime.ToString("f0"); ;
+
+        if (m_GameTime < 0)
+            GameOver();
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Dev/Bradley/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            TogglePause();
+    }
+
+    void TogglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            print("UnPaused");
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            print("Paused");
+        }
+    }
+}
