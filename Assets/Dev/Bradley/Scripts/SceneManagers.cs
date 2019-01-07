4a3fe8de0be9bbbc39d1a16cf3e58aa03a08ba1f
Bradley van Ewijk <527644@student.glu.nl>
2019-01-07 11:04:37 +0100

    New stuff added
    Signed-off-by: Bradley van Ewijk <527644@student.glu.nl>

---
+++ Assets/Dev/Bradley/Scripts/SceneManagers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagers : MonoBehaviour
+{
+    public void LoadOnClick(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void QuitOnClick()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Dev/Luigi/Scripts/PlayerController.cs
+++ Assets/Dev/Luigi/Scripts/PlayerController.cs
@@ -1,20 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-<<<<<<< Updated upstream:Assets/Dev/Luigi/Scripts/temp.cs
-public class temp1 : MonoBehaviour {
-=======
-public class PlayerController : MonoBehaviour {
->>>>>>> Stashed changes:Assets/Dev/Luigi/Scripts/PlayerController.cs
+//<<<<<<< Updated upstream:Assets/Dev/Luigi/Scripts/temp.cs
+//public class temp1 : MonoBehaviour {
+//=======
+//public class PlayerController : MonoBehaviour {
+//>>>>>>> Stashed changes:Assets/Dev/Luigi/Scripts/PlayerController.cs
 
-	// Use this for initialization
-	void Start () {
+//	// Use this for initialization
+//	void Start () {
 		
-	}
+//	}
 	
-	// Update is called once per frame
-	void Update () {
+//	// Update is called once per frame
+//	void Update () {
 		
-	}
-}
+//	}
+//}
