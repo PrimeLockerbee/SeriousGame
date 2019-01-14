8ae697d97d0284de14f4af6b6ebb99eae343f3af
LuigiOvertoom <firedream2236@gmail.com>
2019-01-14 14:35:10 +0100

    fixed animator requirements

--- Assets/Dev/Luigi/Scripts/PotionScript.cs
+++ Assets/Dev/Luigi/Scripts/PotionScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pooling;
 
 public class PotionScript : MonoBehaviour
 {
@@ -8,13 +9,54 @@ public class PotionScript : MonoBehaviour
     public List<GameObject> m_PpotionList;
     public List<GameObject> m_NpotionList;
 
+    public List<ListInList> m_list;
+    [SerializeField]
+    private GameObject m_Pcanvas;
+    [SerializeField]
+    private GameObject m_Ncanvas;
+
+    private int m_listCount = 2;
+    
+    private int m_randomizer;
+
     void Start ()
     {
         m_waterManager = GetComponent<WaterControl>();
+
+        //m_list.Count = m_randomizer;
+
+        for (int i = 0; i < m_list.Count; i++)
+        {
+            if (i == 0)
+            {
+                for (int j = 0; j < m_list[0].Object.Count; j++)
+                {
+                    GameObject obj = (GameObject)Instantiate(m_list[0].Object[j].gameObject, m_Pcanvas.transform);
+                    obj.SetActive(true);
+                    m_PpotionList.Add(obj);
+                }
+            }
+            if (i == 1)
+            {
+                for (int j = 0; j < m_list[1].Object.Count; j++)
+                {
+                    GameObject obj = (GameObject)Instantiate(m_list[1].Object[j].gameObject, m_Ncanvas.transform);
+                    obj.SetActive(true);
+                    m_NpotionList.Add(obj);
+                }
+            }
+        }
     }
 	void Update ()
     {
-	}
+        switch (m_listCount)
+        {
+            case 0:
+                m_NpotionList[0].SetActive(false);
+                m_NpotionList[0].SetActive(true);
+                break;
+        }
+    }
     #region Assign Buttons
     #region Positive
     //Positive Buttons
--- Assets/Dev/Luigi/Scripts/WaterControl.cs
+++ Assets/Dev/Luigi/Scripts/WaterControl.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+enum WaterState
+{
+    schoon,
+    beetjeVies,
+    redelijkVies,
+    ergVies
+}
+
+enum TextState
+{
+    tooPositive,
+    tooNegative
+}
+
 public class WaterControl : MonoBehaviour
 {
+    //Assign State
+    private WaterState m_state;
+    //Assign text State
+    private TextState m_textState;
+    //Assign warning text
+    [SerializeField]
+    //private GameObject m_warningText;
     //Ph management
     public int m_ph;
     private int m_amplitude;
@@ -15,12 +36,9 @@ public class WaterControl : MonoBehaviour
 
     //UI elements
     [SerializeField]
-    private TextMeshPro m_WaterText;
+    private TextMeshProUGUI m_WaterText;
     [SerializeField]
     private GameObject m_phBar;
-
-    //Assign Potion
-    public PotionUseScript m_potion;
 
     void Start ()
     {
@@ -28,7 +46,6 @@ public class WaterControl : MonoBehaviour
 	}
 	void Update ()
     {
-        Debug.Log(m_potion.gameObject.name);
         //Phtext
         m_WaterText.text = m_ph.ToString();
         //PhBar
@@ -63,15 +80,34 @@ public class WaterControl : MonoBehaviour
         //Debug lines
         if (m_ph >= 14)
         {
-            Debug.Log("kijk uit ph is te hoog!");
+            //m_warningText.SetActive(true);
+            m_textState = TextState.tooPositive;
+            if (m_ph <= 20)
+            {
+                m_state = WaterState.beetjeVies;
+            }
+            else
+            {
+                m_state = WaterState.redelijkVies;
+            }
         }
         else if (m_ph <= -12)
         {
-            Debug.Log("kijk uit ph is te laag!");
+            //m_warningText.SetActive(true);
+            m_textState = TextState.tooNegative;
+            if (m_ph <= 20)
+            {
+                m_state = WaterState.redelijkVies;
+            }
+            else
+            {
+                m_state = WaterState.ergVies;
+            }
         }
         else
         {
-            Debug.Log("ph is in balans");
+            //m_warningText.SetActive(false);
+            m_state = WaterState.schoon;
         }
 	}
 }
