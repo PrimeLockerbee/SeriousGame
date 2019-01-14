ebe88beddba85d3664e72d5ab0a1349536a5bcea
Bradley van Ewijk <bradleyvanewijk@ziggo.nl>
2019-01-14 11:49:53 +0100

    Intro Screen added, can switch from intro to the game by key or mouse press.
    Signed-off-by: Bradley van Ewijk <bradleyvanewijk@ziggo.nl>

---
+++ Assets/Dev/Bradley/Scripts/PanelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject introPanel;
+
+    [SerializeField]
+    public GameObject gamePanel;
+
+	void Start ()
+    {
+        introPanel.SetActive(true);
+        gamePanel.SetActive(false);
+	}
+	
+	void Update ()
+    {
+		if (Input.anyKey || Input.GetMouseButton(0))
+        {
+            introPanel.SetActive(false);
+            gamePanel.SetActive(true);
+        }
+	}
+}
